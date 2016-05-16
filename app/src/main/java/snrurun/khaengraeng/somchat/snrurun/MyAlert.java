1c6685f8bb7afb43d88b64ee820b9c20820296ae
natsu <googgig1999@gmail.com>
2016-05-16 01:54:43 -0700

    alert

--- app/src/main/java/snrurun/khaengraeng/somchat/snrurun/MyAlert.java
+++ app/src/main/java/snrurun/khaengraeng/somchat/snrurun/MyAlert.java
@@ -2,6 +2,7 @@ package snrurun.khaengraeng.somchat.snrurun;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 
 /**
  * Created by Administrator on 16/5/2559.
@@ -10,11 +11,21 @@ public class MyAlert {
 
 
     public void myDialod(Context context,
-                         String atrTitle,
+                         String strTitle,
                          String strMessage) {
         AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setCancelable(false);
+        builder.setIcon(R.drawable.doremon48);
+        builder.setTitle(strTitle);
+        builder.setMessage(strMessage);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.show();
     }
 
     }
 
-}
